3a1d6abb983b70bd6ca8f0353c09648116f43bb9
terry <terryspitz@gmail.com>
2026-04-01 16:22:51 +0100

    Remove duplicate test

--- src/generator/tests/FontTest.fs
+++ src/generator/tests/FontTest.fs
@@ -405,29 +405,6 @@ type FontTests() =
             Assert.That(svgStr, Does.Contain("M "), sprintf "Soft corners glyph '%c' should render a moveto" ch)
             Assert.That(svgStr, Does.Not.Contain("NaN"), sprintf "Soft corners glyph '%c' should not contain NaN" ch)
 
-    member this.FilledAxis_ControlsSvgFillStyle() =
-        // When filled=true (and outline=true), SVG should have fill:black.
-        // When filled=false, SVG should have fill:none regardless of outline setting.
-        let filledFont =
-            Font.Font(
-                { Axes.DefaultAxes with
-                    outline = true
-                    filled = true }
-            )
-
-        let unfilledFont =
-            Font.Font(
-                { Axes.DefaultAxes with
-                    outline = true
-                    filled = false }
-            )
-
-        let filledSvg = String.concat " " (filledFont.charToSvg 'o' 0.0 0.0 "black")
-        let unfilledSvg = String.concat " " (unfilledFont.charToSvg 'o' 0.0 0.0 "black")
-
-        Assert.That(filledSvg, Does.Contain("fill:black"), "filled=true should produce fill:black")
-        Assert.That(unfilledSvg, Does.Contain("fill:none"), "filled=false should produce fill:none")
-
     [<Test>]
     member this.DactylSpline_IsLineSegment_HandlesColinearTangents() =
         // Test that DactylSpline.isLineSegment returns true for segments
